d46802402ccf39302d3feccdb9be14ec455ac10f
Paras <paraspidurkar97@gmail.com>
2023-10-11 23:51:45 +0530

    Add files via upload

---
+++ Javascript/03 Array Object/Code Samples/01_02_const_array.js
@@ -0,0 +1,4 @@
+const arr = ["hi there"];
+arr[0] = "new value";
+console.log(arr[0]);
+arr = ["nope, now you are overwriting the array"]
---
+++ Javascript/03 Array Object/Code Samples/3_3_Enum-paradox.js
@@ -0,0 +1,8 @@
+const HANDLER_TYPE = Object.freeze({
+    DASH: "DASH",
+    HLS: "HLS",
+    WAVE: "WAVE",
+    DEFAULT: "DEFAULT"
+});
+
+console.log(HANDLER_TYPE.DASH)
