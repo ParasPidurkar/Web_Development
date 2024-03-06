63599b8bf7f22df7ca73f39e8f6de91b780ae3b0
ParasPidurkar <paraspidurkar97@gmail.com>
2024-03-06 10:29:46 +0530

    props state and context

---
+++ React+React_Native/C03_Props_State_Context/04_default-property-values/src/index.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import MyButton from "./MyButton";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<MyButton />);
---
+++ React+React_Native/C03_Props_State_Context/08_container-components/src/index.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import MyContainer from "./MyContainer";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<MyContainer />);
